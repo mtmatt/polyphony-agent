9c2631881e9c18dbfcb2acfce22bd5e459f885a4
mtmatt <mtmattqq@gmail.com>
2026-05-17 21:13:53 +0800

    chore: initialize Rust project with dependencies

---
+++ src/lib.rs
@@ -0,0 +1,10 @@
+#![allow(dead_code)]
+
+pub mod agent;
+pub mod config;
+pub mod context;
+pub mod engine;
+pub mod recorder;
+pub mod repo_map;
+pub mod state;
+pub mod types;
---
+++ src/main.rs
@@ -0,0 +1,4 @@
+fn main() -> anyhow::Result<()> {
+    println!("polyphony");
+    Ok(())
+}
